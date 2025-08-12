0f167442786f29fd6336d019d994ee631e6e202f
priyanshi srivastava <priyanshi02828@gmail.com>
2025-08-12 21:48:43 +0530

    Initial commit

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,22 @@
-public class Task3 {
-}
+import java.util.Scanner;
+    public class Task3 {
+        public static void main(String[] args) {
+            // Static value version
+            int staticNumber = 27;
+            checkAndPrint(staticNumber);
+
+            // Interactive version
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("\nEnter an integer: ");
+            int userNumber = scanner.nextInt();
+            checkAndPrint(userNumber);
+
+            scanner.close();
+        }
+
+        // Modular method demonstrating ternary operator
+        private static void checkAndPrint(int number) {
+            String result = (number % 2 == 0) ? "Even" : "Odd";
+            System.out.printf("%d is %s\n", number, result);
+        }
+    }
